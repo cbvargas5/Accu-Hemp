a9a1852282b691c8123ce542ad9798fe5ecde771
Darth Varg <cbvargas123@gmail.com>
2020-06-29 20:13:06 -0700

    installed pg & dotenv; created .env and added it to gitignore

--- server/index.js
+++ server/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3131;
 const path = require('path');
+require('dotenv').config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// console.log(process.env.DB_HOST)
 
 app.listen(port, err => err ? console.error('SERVER ERROR: ', err) : console.log(`Server is listening on port ${port}`))
