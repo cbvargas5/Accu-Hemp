4ae714806e3e3ed9ab73d5710bfaa728483a77a9
Darth Varg <cbvargas123@gmail.com>
2020-06-30 17:47:07 -0700

    created two more queries in models/dosages

--- server/database/models/dosages.js
+++ server/database/models/dosages.js
@@ -1,7 +1,7 @@
 const pool = require('../index')
 
 module.exports = {
-    getAll: () => pool.query(`SELECT * FROM dosages`)
-        //Need to add query to grab by condition
-        //Need to add query to grab by condition && Severity
+    getAll: () => pool.query('SELECT * FROM dosages'),
+    getByCondition: (condition) => pool.query(`SELECT * FROM dosages WHERE condition = ${condition}`), //might choose to select severity column rather than entire table
+    getDose: (condition, severity) => pool.query(`SELECT lowerdose, upperdpse FROM dosages WHERE condition = ${condition} AND severity = ${severity}`)
 }
