f264fb01e8d6b7b938194629ef8b73e339961d87
Darth Varg <cbvargas123@gmail.com>
2020-07-05 19:42:37 -0700

    wrote filler code for Carousel component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,6 +3,7 @@ import Header from './Header.jsx';
 import FeatureNav from './FeatureNav.jsx';
 import Footer from './Footer.jsx';
 import IconCard from './cards/IconCard.jsx';
+import Carousel from './Carousel.jsx'
 
 class App extends React.Component {
   constructor(props) {
@@ -23,6 +24,7 @@ class App extends React.Component {
           <IconCard step="1" icon="icon" title="Dose">We can help you estimate your dose of CBD by by answering a few simple questions about your condition and severity/weight. Skip to Step 2 if you already know your dose.</IconCard>
           <IconCard step="2" icon="icon" title="Products">Already know your dose of CBD? Take our Product Finder Quiz to be matched with the best CBD hemp oil products based on your dose. Skip to Step 3 if you already have a product.</IconCard>
           <IconCard step="3" icon="icon" title="Measure">Already know your dose and have your product? Take our Measurement Quiz to help you measure your dose specific to your product! Currently we offer measurements for drops and syringes.</IconCard>
+          <Carousel />
         </section>
         </section>
       </main>
--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function Carousel() {
   return (
-    <div>
-      
-    </div>
+    <section className="brand-carousel-section">
+      <h3>Brands available:</h3>
+      <div className="carousel-wrapper">CAROUSEL WILL GO HERE</div>
+    </section>
   )
 }
