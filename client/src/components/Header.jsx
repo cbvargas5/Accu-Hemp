4a823530f2055a78515913caee756d10e8a4110f
Darth Varg <cbvargas123@gmail.com>
2020-07-07 20:02:49 -0700

    created functions.scss and added function to select all h tags; centered feature-nav

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,11 +4,15 @@ import Navbar from './Navbar.jsx'
 const Header = (props) => (
   <header>
     <div className="header-logo-wrapper">
-      <a href="#">
-        <img src="./images/main-logo.png" alt="Accu-Hemp Main Logo"/>
-      </a>
+      <div className="image-wrapper">
+        <a href="#">
+          <img src="./images/main-logo.png" alt="Accu-Hemp Main Logo"/>
+        </a>
+      </div>
     </div>
-    <Navbar />
+    <div className="main-nav-wrapper">
+      <Navbar />
+    </div>
   </header>
 );
 
