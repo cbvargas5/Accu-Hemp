8b1bfc5d373c7dde229cd201a357c57f8ccfde71
Darth Varg <cbvargas123@gmail.com>
2020-07-10 16:37:13 -0700

    created class for underlining sections and moved it to utilities file

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -20,12 +20,12 @@ class App extends React.Component {
             <h3>Why is using CBD Hemp Oil so <span>complicated?</span></h3>
             <h2>AccuHemp can make it easy:</h2>
           </div>
-        <section className="homepage-steps">
-          <IconCard step="1" icon="icon" title="Dose">We can help you estimate your dose of CBD by by answering a few simple questions about your condition and severity/weight. Skip to Step 2 if you already know your dose.</IconCard>
-          <IconCard step="2" icon="icon" title="Products">Already know your dose of CBD? Take our Product Finder Quiz to be matched with the best CBD hemp oil products based on your dose. Skip to Step 3 if you already have a product.</IconCard>
-          <IconCard step="3" icon="icon" title="Measure">Already know your dose and have your product? Take our Measurement Quiz to help you measure your dose specific to your product! Currently we offer measurements for drops and syringes.</IconCard>
-          <Carousel />
-        </section>
+          <section className="homepage-steps">
+            <IconCard step="1" icon="icon" title="Dose">We can help you estimate your dose of CBD by by answering a few simple questions about your condition and severity/weight. Skip to Step 2 if you already know your dose.</IconCard>
+            <IconCard step="2" icon="icon" title="Products">Already know your dose of CBD? Take our Product Finder Quiz to be matched with the best CBD hemp oil products based on your dose. Skip to Step 3 if you already have a product.</IconCard>
+            <IconCard step="3" icon="icon" title="Measure">Already know your dose and have your product? Take our Measurement Quiz to help you measure your dose specific to your product! Currently we offer measurements for drops and syringes.</IconCard>
+            <Carousel />
+          </section>
         </section>
       </main>
       <Footer />
--- client/src/components/FeatureNav.jsx
+++ client/src/components/FeatureNav.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const Navbar = (props) => (
-  <section className="feature-nav-section">
+  <section className="feature-nav-section underline-sec">
     <nav className="feature-nav">
       {/* Experiment Naver class */}
-      <ul className="naver">
+      <ul>
         <li><span className="steps-nav">Step 1</span><br/>DOSE</li>
         <li><span className="steps-nav">Step 2</span><br/>PRODUCTS</li>
         <li><span className="steps-nav">Step 3</span><br/>MEASURE</li>
