c7e4fed994669981cbd2209b2dd40abf98bd1828
Darth Varg <cbvargas123@gmail.com>
2020-07-11 13:42:28 -0700

    formatted homepage-steps to flex; Moved Carousel to its own section

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -24,8 +24,8 @@ class App extends React.Component {
             <IconCard step="1" icon="icon" title="Dose">We can help you estimate your dose of CBD by by answering a few simple questions about your condition and severity/weight. Skip to Step 2 if you already know your dose.</IconCard>
             <IconCard step="2" icon="icon" title="Products">Already know your dose of CBD? Take our Product Finder Quiz to be matched with the best CBD hemp oil products based on your dose. Skip to Step 3 if you already have a product.</IconCard>
             <IconCard step="3" icon="icon" title="Measure">Already know your dose and have your product? Take our Measurement Quiz to help you measure your dose specific to your product! Currently we offer measurements for drops and syringes.</IconCard>
-            <Carousel />
           </section>
+          <Carousel />
         </section>
       </main>
       <Footer />
