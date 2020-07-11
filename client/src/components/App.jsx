16235ca2eb66afab9c178162842e6480dcb07942
Darth Varg <cbvargas123@gmail.com>
2020-07-11 13:49:38 -0700

    imported icons to steps and import solid.css file from font awesome;  added other styling to mid section

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -21,9 +21,9 @@ class App extends React.Component {
             <h2>AccuHemp can make it easy:</h2>
           </div>
           <section className="homepage-steps">
-            <IconCard step="1" icon="icon" title="Dose">We can help you estimate your dose of CBD by by answering a few simple questions about your condition and severity/weight. Skip to Step 2 if you already know your dose.</IconCard>
-            <IconCard step="2" icon="icon" title="Products">Already know your dose of CBD? Take our Product Finder Quiz to be matched with the best CBD hemp oil products based on your dose. Skip to Step 3 if you already have a product.</IconCard>
-            <IconCard step="3" icon="icon" title="Measure">Already know your dose and have your product? Take our Measurement Quiz to help you measure your dose specific to your product! Currently we offer measurements for drops and syringes.</IconCard>
+            <IconCard step="1" icon="fas fa-laptop-medical" title="Dose">We can help you estimate your dose of CBD by by answering a few simple questions about your condition and severity/weight. Skip to Step 2 if you already know your dose.</IconCard>
+            <IconCard step="2" icon="fas fa-shopping-cart" title="Products">Already know your dose of CBD? Take our Product Finder Quiz to be matched with the best CBD hemp oil products based on your dose. Skip to Step 3 if you already have a product.</IconCard>
+            <IconCard step="3" icon="fas fa-syringe" title="Measure">Already know your dose and have your product? Take our Measurement Quiz to help you measure your dose specific to your product! Currently we offer measurements for drops and syringes.</IconCard>
           </section>
           <Carousel />
         </section>
