9e31eb2f5b7272592ab11fd5dc5efcda43caaa13
Darth Varg <cbvargas123@gmail.com>
2020-07-11 19:26:00 -0700

    completed footer; underlined steps section

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -20,7 +20,7 @@ class App extends React.Component {
             <h3>Why is using CBD Hemp Oil so <span>complicated?</span></h3>
             <h2>AccuHemp can make it easy:</h2>
           </div>
-          <section className="homepage-steps">
+          <section className="homepage-steps underline-sec">
             {/* add link prop */}
             <IconCard step="1" icon="fas fa-laptop-medical" title="Dose">We can help you estimate your dose of CBD by by answering a few simple questions about your condition and severity/weight. Skip to Step 2 if you already know your dose.</IconCard>
             <IconCard step="2" icon="fas fa-shopping-cart" title="Products">Already know your dose of CBD? Take our Product Finder Quiz to be matched with the best CBD hemp oil products based on your dose. Skip to Step 3 if you already have a product.</IconCard>
