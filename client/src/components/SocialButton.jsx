d1b3a2b0a0c8b5dd1d3d5e00500bae8ffd0d87c7
Darth Varg <cbvargas123@gmail.com>
2020-07-11 19:02:26 -0700

    refactor SocialButton component to only include anchor tag rather than div

--- client/src/components/SocialButton.jsx
+++ client/src/components/SocialButton.jsx
@@ -2,10 +2,8 @@ import React from 'react'
 
 export default function SocialButton(props) {
   return (
-    <div className="social-btn">
-      <a href={props.link} target="_blank">
-        <i className={props.icon}></i>
-      </a>
-    </div>
+    <a className="social-btn" href={props.link} target="_blank">
+      <i className={props.icon}></i>
+    </a>
   )
 }
