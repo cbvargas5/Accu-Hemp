9383452c9f82ebc7f84275e8f389855bbb252f97
Darth Varg <cbvargas123@gmail.com>
2020-07-12 11:45:55 -0700

    added images

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import Carousel from 'react-bootstrap/Carousel'
 
-export default function Carousel() {
+export default function() {
   return (
-    <section className="brand-carousel-section">
-      <h3>Brands available:</h3>
-      <div className="carousel-wrapper">CAROUSEL WILL GO HERE</div>
-    </section>
+    <Carousel>
+      <Carousel.Item>
+        <img src="./images/" alt=""/>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src="./images/" alt=""/>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src="./images/" alt=""/>
+      </Carousel.Item>
+    </Carousel>
+
+
+    // <section className="brand-carousel-section">
+    //   <h3>Brands available:</h3>
+    //   <div className="carousel-wrapper">CAROUSEL WILL GO HERE</div>
+    // </section>
   )
 }
