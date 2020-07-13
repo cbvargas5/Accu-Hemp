080d451a9e5e59b3670f3a33e5927e47286ebcf7
Darth Varg <cbvargas123@gmail.com>
2020-07-12 20:37:23 -0700

    included Carousel into section tag of Carousel component; reset img width

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -5,7 +5,9 @@ export default function() {
   const PATH = `./images/${"Brand logos"}/`
   const BRANDS = ['cbdmd', 'cw', 'elixinol', 'elsol', 'endoca', 'incann', 'joy_organics']
   return (
-    <Carousel>
+    <section className="brand-carousel-section">
+      <h3>Brands available:</h3>
+      <Carousel>
       {
         BRANDS.map((brand, index) => (
           <Carousel.Item key={index}>
@@ -13,18 +15,8 @@ export default function() {
           </Carousel.Item>
         ))
       }
-      {/* <Carousel.Item>
-        <img src="./images/" alt=""/>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img src="./images/" alt=""/>
-      </Carousel.Item> */}
-    </Carousel>
-
-
-    // <section className="brand-carousel-section">
-    //   <h3>Brands available:</h3>
-    //   <div className="carousel-wrapper">CAROUSEL WILL GO HERE</div>
-    // </section>
+      </Carousel>
+      {/* <div className="carousel-wrapper">CAROUSEL WILL GO HERE</div> */}
+    </section>
   )
 }
