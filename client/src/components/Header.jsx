e699afd1d0bf7f9b1eff878d886f9485f40dde50
Darth Varg <cbvargas123@gmail.com>
2020-07-16 23:09:18 -0700

    Logo is now a functioning link

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import Navbar from './Navbar.jsx'
+import { Link } from 'react-router-dom';
 
 const Header = (props) => (
   <header>
     <div className="header-logo-wrapper">
       <div className="image-wrapper">
-        <a href="#">
-          <img src="./images/main-logo.png" alt="Accu-Hemp Main Logo"/>
-        </a>
+        <Link to="/"><img src="./images/main-logo.png" alt="Accu-Hemp Main Logo"/></Link>
       </div>
     </div>
     <div className="main-nav-wrapper">
