4fce4d9c61ad7a2f3b84d97ff5b94df9d0832dc6
Darth Varg <cbvargas123@gmail.com>
2020-07-16 22:55:04 -0700

    added products & measure routes

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Homepage from './pages/Homepage.jsx';
 import DosePage from './pages/DosePage.jsx';
+import ProductsPage from './pages/ProductsPage.jsx';
+import MeasurePage from './pages/MeasurePage.jsx';
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
 import FeatureNav from './FeatureNav.jsx';
@@ -22,6 +24,8 @@ class App extends React.Component {
             <Switch>
               <Route path="/" exact component={Homepage} />
               <Route path="/dose" exact component={DosePage} />
+              <Route path="/products" exact component={ProductsPage} />
+              <Route path="/measure" exact component={MeasurePage} />
             </Switch>
           </section>
         </BrowserRouter>
