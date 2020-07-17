94e33dcfe793c31d702507e6932ac0418da98519
Darth Varg <cbvargas123@gmail.com>
2020-07-17 15:15:48 -0700

    started Condition tab but will convert header to its own component

--- client/src/components/pages/Dose/Estimator/ConditionTab.jsx
+++ client/src/components/pages/Dose/Estimator/ConditionTab.jsx
@@ -3,9 +3,14 @@ import React, { Component } from 'react'
 export default class ConditionTab extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <section className="estimator-wrapper">
+        <div>
+          <h2>Dose Estimator</h2>
+          <div className="progressbar-wrapper">
+            <p></p>
+          </div>
+        </div>
+      </section>
     )
   }
 }
