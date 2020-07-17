8b1e09129fbcf1f0c88695f433ef81cf4aa2a688
Darth Varg <cbvargas123@gmail.com>
2020-07-17 12:07:57 -0700

    inserted icon and icon prop

--- client/src/components/cards/ButtonCard.jsx
+++ client/src/components/cards/ButtonCard.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function ButtonCard(props) {
   return (
     <div className="btn-card">
-      <Link to={props.link}>{props.children}</Link>
+      <Link to={props.link}>{props.children}<i className={props.icon}></i></Link>
     </div>
   )
 }
