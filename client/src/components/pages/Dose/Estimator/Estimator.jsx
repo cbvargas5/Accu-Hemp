a7fe4179f76903a5672efefca15646cfa690a0da
Darth Varg <cbvargas123@gmail.com>
2020-07-17 17:52:27 -0700

    added constructor to Estimator; wrote pseudocode

--- client/src/components/pages/Dose/Estimator/Estimator.jsx
+++ client/src/components/pages/Dose/Estimator/Estimator.jsx
@@ -2,14 +2,27 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import ProgressTracker from '../../../ProgressTracker.jsx'
 import { Link } from 'react-router-dom'
+import TabCondition from './TabCondition.jsx'
+import TabSeverity from './TabSeverity.jsx'
+import TabResults from './TabResults.jsx'
 
 
 export class Estimator extends Component {
+  constructor(props) {
+    super(props)
+
+    const first = TabCondition
+    const second = TabSeverity
+    const third = TabResults
+  }
   render() {
     return (
       <section className="estimator-wrapper">
         <ProgressTracker title="Dose Estimator" currStep={1} lastStep={3}/>
-        <Link to="/">Estimator</Link>
+        {/* Need to set up sub routes here to conditionally
+            render different tabs */}
+        
+        {/* Need to map over condition array from state that was grabbed from database */}
       </section>
     )
   }
