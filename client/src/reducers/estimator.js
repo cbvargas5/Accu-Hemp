a0957f25cdcbe2c30fe0d3133737b4f434dd4ce9
Darth Varg <cbvargas123@gmail.com>
2020-07-18 17:00:57 -0700

    updated estimator and reducer

--- client/src/reducers/estimator.js
+++ client/src/reducers/estimator.js
@@ -2,14 +2,14 @@ import { GET_DOSE } from '../constants/action-types'
 
 
 const initialState = {
-
+    doses: []
 }
 
 export default (state = initialState, { type, payload }) => {
     switch (type) {
 
         case GET_DOSE:
-            return {...state, ...payload }
+            return {...state, doses: [...payload] }
 
         default:
             return state
