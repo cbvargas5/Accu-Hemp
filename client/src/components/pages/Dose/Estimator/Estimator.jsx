7435da59d18bd284a6596ca74b221c8cad5027ca
Darth Varg <cbvargas123@gmail.com>
2020-07-19 21:17:16 -0700

    imported functions from react router dom; set up switches and commented them out

--- client/src/components/pages/Dose/Estimator/Estimator.jsx
+++ client/src/components/pages/Dose/Estimator/Estimator.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 
 import ProgressTracker from '../../../ProgressTracker.jsx'
 import TabCondition from './TabCondition.jsx'
@@ -20,9 +23,14 @@ class Estimator extends Component {
         <ProgressTracker title="Dose Estimator" currStep={1} lastStep={3}/>
         {/* Need to set up sub routes here to conditionally
             render different tabs */}
-      <TabCondition />
-      {/* <TabSeverity />
-      <TabResults /> */}
+        {/* <Switch>
+          <Route path="/" exact component={TabCondition} />
+          <Route path="/dose" exact component={TabSeverity} />
+          <Route path="/dose/estimator" exact component={TabResults} />
+        </Switch> */}
+        <TabCondition />
+        <Button>Previous</Button>
+        <Button>Next</Button>
       </section>
     )
   }
