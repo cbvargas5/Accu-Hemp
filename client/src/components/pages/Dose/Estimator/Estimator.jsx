d15eb90f3c0253be7f47958111fd92b3c3f3d296
Darth Varg <cbvargas123@gmail.com>
2020-07-19 17:54:04 -0700

    moved necessary code to TabResults component

--- client/src/components/pages/Dose/Estimator/Estimator.jsx
+++ client/src/components/pages/Dose/Estimator/Estimator.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { getConditions } from '../../../../actions/dose'
 
 import ProgressTracker from '../../../ProgressTracker.jsx'
 import TabCondition from './TabCondition.jsx'
@@ -14,24 +13,14 @@ class Estimator extends Component {
     super(props)
 
   }
-  componentDidMount() {
-    this.props.getConditions()
-      .then(data => {
-        console.log('after action -->', this.props)
-      })
-  }
+
   render() {
     return (
       <section className="estimator-wrapper">
         <ProgressTracker title="Dose Estimator" currStep={1} lastStep={3}/>
         {/* Need to set up sub routes here to conditionally
             render different tabs */}
-        <ul>
-          {this.props.estimator.conditions
-            .map(({ condition }, index) => (
-              <li key={index}>{condition}</li>
-            ))}
-        </ul>
+      <TabResults />
       </section>
     )
   }
--- client/src/components/pages/Dose/Estimator/TabResults.jsx
+++ client/src/components/pages/Dose/Estimator/TabResults.jsx
@@ -1,11 +1,39 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default class TabResults extends Component {
+import { getConditions } from '../../../../actions/dose'
+
+
+export class TabResults extends Component {
+  constructor(props) {
+    super(props)
+  }
+  componentDidMount() {
+    this.props.getConditions()
+      .then(data => {
+        console.log('after action -->', this.props)
+      })
+  }
   render() {
     return (
       <div>
-        
+        <ul>
+          {this.props.estimator.conditions
+            .map(({ condition }, index) => (
+              <li key={index}>{condition}</li>
+            ))}
+        </ul>
       </div>
     )
   }
 }
+
+const mapStateToProps = (state) => ({
+  
+})
+
+const mapDispatchToProps = {
+  
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TabResults)
