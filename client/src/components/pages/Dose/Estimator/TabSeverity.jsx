936592793648d2c267375a40d691d0eb687ed1d7
Darth Varg <cbvargas123@gmail.com>
2020-07-20 17:02:18 -0700

    api call on severityTab mount; severities rendering

--- client/src/components/pages/Dose/Estimator/TabSeverity.jsx
+++ client/src/components/pages/Dose/Estimator/TabSeverity.jsx
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { getSeverities, selectSeverity } from '../../../../actions/dose'
+
 
 export class TabSeverity extends Component {
+  constructor(props) {
+    super(props)
+  }
+  componentDidMount() {
+    this.props.getSeverities()
+  }
   render() {
     console.log('Severity props ->', this.props)
     return (
       <div>
-        <h2>Severity Section</h2>
+        <ul>
+          {this.props.estimator.severities.map(({ severity, id }) => (
+            <li key={id}>
+              <input type="button" value={severity}/>
+            </li>
+          ))}
+        </ul>
       </div>
     )
   }
@@ -17,4 +31,4 @@ const mapStateToProps = (state) => ({
 })
 
 
-export default connect(mapStateToProps)(TabSeverity)
+export default connect(mapStateToProps, { getSeverities, selectSeverity })(TabSeverity)
