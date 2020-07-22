0caf04c3e465d90abfa154227c129c4e585c92d6
Darth Varg <cbvargas123@gmail.com>
2020-07-21 17:43:49 -0700

    added result section of result tab

--- client/src/components/pages/Dose/Estimator/TabResults.jsx
+++ client/src/components/pages/Dose/Estimator/TabResults.jsx
@@ -17,8 +17,19 @@ export class TabResults extends Component {
       const { lowerdose, upperdose } = dose[0]
       return (
         <div className="tab results-tab">
-          <h2>Results section</h2>
-          <p>Your estimated dose is {lowerdose} to {upperdose}+ mg of CBD</p>
+          <section className="result">
+            <h2>Your estimated dose is {lowerdose} to {upperdose}+ mg of CBD</h2>
+            <br/>
+            <p>Based on your condition and the severity of your condition, this calculator estimates a dose of <strong>{lowerdose} to {upperdose}mg of CBD each day</strong>. A common technique in CBD hemp oil is to "start low and go slow." Try starting with mg each day and gradually increase your dose until you feel relief. For all-day relief try splitting up your dose throughout day.</p>
+            <br/>
+            <p className="result-note"><strong>Please note:</strong> Your optimal dose or doctor recommended dose may be different from the dose that we calculated. <strong>We advise you to speak with a healthcare professional regarding your use of CBD hemp oil, the dosing of CBD hemp oil, and the interactions CBD hemp oil may have on your other medications.</strong> This calculator is only meant to be a general guide, and should not be considered medical advice. The information from this Dose Estimator is from crowd-sourced and research data obtained from cbddosagecalculator.com. These statements have not been evaluated by the Food and Drug Administration (FDA) and are not intended to diagnose, treat, cure or prevent any disease. Please consult with a medical professional for any medical advice.</p>
+          </section>
+          <section className="redirect">
+
+          </section>
+          <section className="email-results">
+
+          </section>
         </div>
       )
     } else {
