a18e293eae17128b41f16e8dcf59dc37c685991f
Darth Varg <cbvargas123@gmail.com>
2020-07-27 10:25:06 -0700

    added redirect button section to products

--- client/src/components/pages/ProductsPage.jsx
+++ client/src/components/pages/ProductsPage.jsx
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
+import ButtonCard from '../cards/ButtonCard.jsx'
+import { Link } from 'react-router-dom'
+
+
 
 export default class ProductsPage extends Component {
   render() {
     return (
       <div>
-        <h2>This is the Products page</h2>
+        <section className="mini-header">
+          <h2>Products</h2>
+        </section>
+        <section className="quiz-nav">
+          <ButtonCard link="/product-finder" icon="fas fa-search-dollar">Product Finder Quiz</ButtonCard>
+          <p>We have handpicked all the products in our shop and have personally tested most of them. All of these products have quality test results linked in their descriptions.</p>
+          <p><strong>Please note:</strong> We may earn a small commission if you purchase products through the links on our website.</p>
+        </section>
+        <section className="product-list">
+
+        </section>
       </div>
     )
   }
 }
+
