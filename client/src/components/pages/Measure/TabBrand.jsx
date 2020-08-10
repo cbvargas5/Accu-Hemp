21cad285a2d4100568ee1d8c4a91c836436e5b79
Darth Varg <cbvargas123@gmail.com>
2020-08-10 16:43:27 -0700

    created filter method

--- client/src/components/pages/Measure/TabBrand.jsx
+++ client/src/components/pages/Measure/TabBrand.jsx
@@ -11,18 +11,29 @@ class TabBrand extends Component {
   constructor(props) {
     super(props)
 
+  }
+  filterBrands = ({ brand }) => {
+    if (brand in this) {
+      return false
+    } else {
+      this[brand] = brand
+      return true
+    }
   }  
   handleSelection = (e) => {
     this.props.selectBrand({selectedBrand: e.target.value})
   }
   render() {
-    console.log('test ->', this.props)
+    console.log('brand props ->', this.props)
     return (
       <div className="tab">
         <div className="question-line">
           <h4>Select Brand:*</h4>
         </div>
         <div className="device-wrapper">
+          {
+            this.props.products.filter(this.filterBrands, {}).map(() => <li>brand</li>)
+          }
           <Button onClick={(e) => { this.props.onNext(); this.handleSelection(e); }} value="dropper" className="tab-btn">Dropper <i className="fas fa-eye-dropper"></i></Button>
           <Button onClick={(e) => { this.props.onNext(); this.handleSelection(e); }} value="syringe" className="tab-btn">Syringe <i className="fas fa-syringe"></i></Button>
         </div>
