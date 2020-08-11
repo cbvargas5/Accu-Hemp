c07003fb9df40acb30a444bd15e556c9f493864c
Darth Varg <cbvargas123@gmail.com>
2020-08-11 16:48:15 -0700

    added working calculations to measure results

--- client/src/components/pages/Measure/TabMeasureResults.jsx
+++ client/src/components/pages/Measure/TabMeasureResults.jsx
@@ -6,17 +6,22 @@ class TabMeasureResults extends Component {
     super(props)
 
   }
-  calculateMeasurement = (selectedDose, productMg, selectedSize) => {
-
+  calculateMeasurement = (device, userDose, productMg, productSize) => {
+    if (device === 'syringe') {
+     return `${Math.round(((productSize/productMg) * userDose) * 10) / 10} mL`
+    } else if (device === 'dropper') {
+     return `${Math.round(userDose /((productMg / productSize) *.0333))} drops`
+    }
   }
   render() {
     console.log('results: ', this.props)
+    const measurement = this.calculateMeasurement(this.props.selectedDevice, this.props.selectedDose, this.props.selectedProductMg, this.props.selectedSize)
     return (
       <section className="tab results-tab">
       <section className="result mini-underline-sec">
-        <h2>Your estimated measurement is 3.6 mL</h2>
+      <h2>Your estimated measurement is {measurement}</h2>
         <br/>
-        <p>For {`${this.props.selectedBrand} ${this.props.selectedProduct}`} you would need approximately 3.6 mL to measure {this.props.selectedDose} mg of CBD.</p>
+        <p>For {`${this.props.selectedBrand} ${this.props.selectedProduct}`} you would need approximately {measurement} to measure {this.props.selectedDose} mg of CBD.</p>
         <br/>
         <p className="result-note"><strong>Please note:</strong> This measurement is just an approximation. The exact number of drops depends on the size of your dropper and how accurate the product's labeling is. We advise you to speak with a healthcare professional regarding your use of CBD hemp oil. This calculator is only meant to be a general guide, and should not be considered medical advice. These statements have not been evaluated by the Food and Drug Administration (FDA) and are not intended to diagnose, treat, cure or prevent any disease. Please consult with a medical professional for any medical advice.</p>
       </section>
