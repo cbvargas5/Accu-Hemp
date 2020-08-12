fc0a401d27fdf32730744fb376f54d7e398f916a
Darth Varg <cbvargas123@gmail.com>
2020-08-11 17:03:01 -0700

    changed nav to links

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+// const pages = [['Home', '/path'], ['Companies', '/path'], ['Help', '/path'], ['Contact Us', '/path'], ['Legal', '/path'], ['Accugentix', '/path']]
 
 const Navbar = (props) => (
   <nav className="main-nav">
     <ul>
-      <li>Home</li>
-      <li>Companies</li>
-      <li>Help</li>
-      <li>Contact Us</li>
-      <li>Legal</li>
-      <li>Accugentix</li>
+      <li><Link to="">Home</Link></li>
+      <li><Link to="">Companies</Link></li>
+      <li><Link to="">Help</Link></li>
+      <li><Link to="">Contact Us</Link></li>
+      <li><Link to="">Legal</Link></li>
+      <li><a href="https://accugentix.com/" target="_blank">Accugentix</a></li>
     </ul>
   </nav>
 );
