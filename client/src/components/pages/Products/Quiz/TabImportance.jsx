2d81711be9303a034f78fc3036f0f50fda954dc2
Darth Varg <cbvargas123@gmail.com>
2020-08-18 17:50:32 -0700

    filtered by dose with new method

--- client/src/components/pages/Products/Quiz/TabImportance.jsx
+++ client/src/components/pages/Products/Quiz/TabImportance.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { selectImportance } from '../../../../actions/products'
+import { selectImportance, updateFilteredProducts } from '../../../../actions/products'
 
 import Option from './Option.jsx'
 
@@ -8,6 +8,31 @@ import Option from './Option.jsx'
 class TabImportance extends Component {
   constructor(props) {
     super(props)
+  }
+  filterByDose = (product) => {
+    const mgPerMl = product.milligrams / product.size
+
+    switch(this.props.selectedDose) {
+      case 'Less than 10 mg of CBD': 
+        return mgPerMl >= 7 && mgPerMl <= 25
+      case '10 to 20 mg of CBD': 
+        return mgPerMl >= 10 && mgPerMl <= 50
+      case '20 to 30 mg of CBD': 
+        return mgPerMl >= 25 && mgPerMl <= 60
+      case '30 to 50 mg of CBD': 
+        return mgPerMl >= 30 && mgPerMl <= 100
+      case '50 to 75 mg of CBD': 
+        return mgPerMl >= 50 && mgPerMl <= 167
+      case '75 to 100 mg of CBD': 
+        return mgPerMl >= 60 && mgPerMl <= 250
+      case '100 to 200 mg of CBD': 
+        return mgPerMl >= 100 && mgPerMl <= 333
+      case 'More than 200 mg of CBD': 
+        return mgPerMl >= 150 && mgPerMl <= 333
+    }
+  }
+  componentDidMount() {
+    this.props.updateFilteredProducts(this.props.filteredProducts.filter(this.filterByDose))
   }
   handleSelection = (e) => {
     this.props.selectImportance({selectedImportance: e.target.value})
@@ -32,4 +57,4 @@ class TabImportance extends Component {
 const mapStateToProps = (state) => ({...state.products})
 
 
-export default connect(mapStateToProps, { selectImportance })(TabImportance)
+export default connect(mapStateToProps, { selectImportance, updateFilteredProducts })(TabImportance)
