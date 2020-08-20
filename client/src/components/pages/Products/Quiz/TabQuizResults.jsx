596c3ce024a38050c687fc26b244d90e02162954
Darth Varg <cbvargas123@gmail.com>
2020-08-19 22:54:16 -0700

    created working filter

--- client/src/components/pages/Products/Quiz/TabQuizResults.jsx
+++ client/src/components/pages/Products/Quiz/TabQuizResults.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { updateFilteredProducts } from '../../../../actions/products'
+
 
 import ProductCard from '../../../cards/ProductCard.jsx'
 
@@ -7,7 +9,25 @@ class TabQuizResults extends Component {
   constructor(props) {
     super(props)
   }
+  filterByQuality = ({ melatonin, spectrum, thc, organic, flavor }) => {
+
+    switch(this.props.selectedQualities) {
+      case 'Sleep Aid (CBD + Melatonin)': 
+        return melatonin
+      case 'Full/Broad Spectrum products': 
+        return spectrum
+      case 'Zero THC products': 
+        return !thc
+      case 'From organically grown hemp': 
+        return organic
+      case 'Different flavor options available': 
+        return flavor
+      case 'Any of these': 
+        return true
+    }
+  }
   componentDidMount() {
+    this.props.updateFilteredProducts(this.props.filteredProducts.filter(this.filterByQuality))
     window.scrollTo(0, 0)
   }
   render() {
@@ -26,4 +46,4 @@ class TabQuizResults extends Component {
 const mapStateToProps = (state) => ({...state.products})
 
 
-export default connect(mapStateToProps)(TabQuizResults)
+export default connect(mapStateToProps, { updateFilteredProducts })(TabQuizResults)
