4bf459d83d13be9a6e1736f3d7b4e011df147218
Darth Varg <cbvargas123@gmail.com>
2020-08-22 21:57:26 -0700

    added html to legal page

--- client/src/components/pages/LegalPage.jsx
+++ client/src/components/pages/LegalPage.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 export default function LegalPage() {
   return (
-    <div>
-      <h2>Legal</h2>
-    </div>
+    <section>
+      <div className="mini-header">
+        <h1>Contact Us</h1>
+      </div>
+      <h3>User Agreement:</h3>
+      <p><strong>User Agreement that you must read, understand, and agree to in order to use AccuHemp</strong>&trade;</p>
+      <p><strong>1)</strong> You must be at least <strong>18 years old</strong> to use AccuHemp&trade;.</p>
+      <p><strong>2)</strong> The statements on AccuHemp&trade; have not been evaluated by the Food and Drug Administration (FDA) and are not intended to diagnose, treat, cure or prevent any disease.</p>
+      <p><strong>3)</strong> The content on this website/app should not be considered medical advice and we recommend that CBD hemp oil users consult with a medical professional for any medical advice.</p>
+      <p><strong>4)</strong> AccuHemp™ is a product of Accugentix LLC, and Accugentix LLC is not responsible or liable for any misuse of content, injury to users, or damages caused by the content on AccuHemp™ and linked external websites.</p>
+      <p><strong>5)</strong> Accugentix LLC may earn a small commission when users click on and/or purchase from external websites linked from AccuHemp&trade;.</p>
+      <p><strong>6)</strong> AccuHemp™ uses cookies. Our legal Disclaimers, Privacy Policy, Terms of Use, and Cookie Policy can all be found under the “Legal” tab on the website.</p>
+    </section>
   )
 }
