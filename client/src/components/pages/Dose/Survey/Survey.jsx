bdb7560320819b66bb5038cd719824587ef9cf25
Darth Varg <cbvargas123@gmail.com>
2020-08-24 21:45:28 -0700

    created survey file

---
+++ client/src/components/pages/Dose/Survey/Survey.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Survey extends Component {
+  render() {
+    return (
+      <div>
+        hello from Survey
+      </div>
+    )
+  }
+}
