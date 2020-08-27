7075be12010679070214a46b7bf35a86839ab4c1
Darth Varg <cbvargas123@gmail.com>
2020-08-26 23:38:45 -0700

    added modal file

---
+++ client/src/components/Modal.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+
+
+export default class Modal extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Modal time!</h2>
+      </div>
+    )
+  }
+}
