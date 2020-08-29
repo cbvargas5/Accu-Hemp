d81ec2c8d9d8eb0a5c4629c376ce770e82688ea5
Darth Varg <cbvargas123@gmail.com>
2020-08-29 13:13:31 -0700

    created reducer file for survey

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux'
 import home from './home'
 import estimator from './estimator'
+import survey from './survey'
 import products from './products'
 import measure from './measure'
 
@@ -8,6 +9,7 @@ import measure from './measure'
 const rootReducer = combineReducers({
     home,
     estimator,
+    survey,
     products,
     measure
 });
