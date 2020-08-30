d2f4db3893cdf014b837477106db4e9df9de8e68
Darth Varg <cbvargas123@gmail.com>
2020-08-29 18:17:52 -0700

    completed reducers for survey

--- client/src/reducers/survey.js
+++ client/src/reducers/survey.js
@@ -1,11 +1,53 @@
+import { GET_CONDITIONS, UPDATE_SURVEY_STEPS, SELECT_CONDITION, INPUT_WEIGHT, SELECT_PROFESSIONAL_HELP, SELECT_DOSE, SELECT_OTHER_MEDICATION, SELECT_DOSE_DURATION, SELECT_IMPROVEMENT, INPUT_ELABORATE, VERIFY_SURVEY } from '../constants/action-types'
+
+
 const initialState = {
-
+    step: 1,
+    conditions: [],
+    inputWeight: '',
+    selectedDose: '',
+    selectedCondition: '',
+    selectedProfessionalHelp: '',
+    selectedOtherMedicication: '',
+    selectedDoseDuration: '',
+    selectedImprovement: '',
+    inputElaborate: '',
+    selectedVerification: '',
 }
-
 export default (state = initialState, { type, payload }) => {
     switch (type) {
 
-        case typeName:
+        case GET_CONDITIONS:
+            return {...state, conditions: [...payload] }
+
+        case UPDATE_SURVEY_STEPS:
+            return {...state, ...payload }
+
+        case INPUT_WEIGHT:
+            return {...state, ...payload }
+
+        case SELECT_DOSE:
+            return {...state, ...payload }
+
+        case SELECT_CONDITION:
+            return {...state, ...payload }
+
+        case SELECT_PROFESSIONAL_HELP:
+            return {...state, ...payload }
+
+        case SELECT_OTHER_MEDICATION:
+            return {...state, ...payload }
+
+        case SELECT_DOSE_DURATION:
+            return {...state, ...payload }
+
+        case SELECT_IMPROVEMENT:
+            return {...state, ...payload }
+
+        case INPUT_ELABORATE:
+            return {...state, ...payload }
+
+        case VERIFY_SURVEY:
             return {...state, ...payload }
 
         default:
