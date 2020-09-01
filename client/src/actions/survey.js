b4ecd2b2e00a23d0786ca5e6eaff034f97e09284
Darth Varg <cbvargas123@gmail.com>
2020-08-31 22:54:03 -0700

    created the rest of the actions for survey

--- client/src/actions/survey.js
+++ client/src/actions/survey.js
@@ -9,3 +9,53 @@ export const getConditionsForSurvey = () => (dispatch, getState) => {
         .then(({ data }) => dispatch({ type: GET_CONDITIONS, payload: data }))
         .catch(err => console.log('failed to get CONDITIONS. ERROR: ', err))
 }
+
+export const updateSurveySteps = (payload) => ({
+    type: UPDATE_SURVEY_STEPS,
+    payload
+})
+
+export const selectSurveyCondition = (payload) => ({
+    type: SELECT_CONDITION,
+    payload
+})
+
+export const inputWeight = (payload) => ({
+    type: INPUT_WEIGHT,
+    payload
+})
+
+export const selectProfessionalHelp = (payload) => ({
+    type: SELECT_PROFESSIONAL_HELP,
+    payload
+})
+
+export const selectSurveyDose = (payload) => ({
+    type: SELECT_DOSE,
+    payload
+})
+
+export const selectOtherMedication = (payload) => ({
+    type: SELECT_OTHER_MEDICATION,
+    payload
+})
+
+export const selectDoseDuration = (payload) => ({
+    type: SELECT_DOSE_DURATION,
+    payload
+})
+
+export const selectImprovement = (payload) => ({
+    type: SELECT_IMPROVEMENT,
+    payload
+})
+
+export const inputElaborate = (payload) => ({
+    type: INPUT_ELABORATE,
+    payload
+})
+
+export const verifySurvey = (payload) => ({
+    type: VERIFY_SURVEY,
+    payload
+})
