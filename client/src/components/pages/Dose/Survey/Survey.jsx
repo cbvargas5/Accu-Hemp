148027c99a7aca00da5e36324bad5569993e3980
Darth Varg <cbvargas123@gmail.com>
2020-09-01 23:05:29 -0700

    imported all tabs to survey component

--- client/src/components/pages/Dose/Survey/Survey.jsx
+++ client/src/components/pages/Dose/Survey/Survey.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 
+import TabExtras from './TabExtras.jsx'
+import TabHowMuch from './TabHowMuch.jsx'
+import TabSubmit from './TabSubmit.jsx'
+import TabSurveyCondition from './TabSurveyCondition.jsx'
+import TabWeightSeverity from './TabWeightSeverity.jsx'
+
 export default class Survey extends Component {
   render() {
     return (
       <div>
-        <h2>hello from Survey</h2>
+        {/* Need to render survey tabs */}
       </div>
     )
   }
