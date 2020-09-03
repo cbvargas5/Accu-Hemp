defd2f2aff43089e7dc1a0538469101314478859
Darth Varg <cbvargas123@gmail.com>
2020-09-02 21:02:04 -0700

    copied next and previous button methods from estimator

--- client/src/components/pages/Dose/Survey/Survey.jsx
+++ client/src/components/pages/Dose/Survey/Survey.jsx
@@ -7,6 +7,34 @@ import TabSurveyCondition from './TabSurveyCondition.jsx'
 import TabWeightSeverity from './TabWeightSeverity.jsx'
 
 export default class Survey extends Component {
+  constructor(props) {
+    super(props)
+
+  }
+  componentDidMount() {
+    this.props.history.push(`${this.props.match.url}/${this.props.estimator.step}`)
+  }
+  onNext = () => {
+    const { step } = this.props.estimator
+    if (step < 3) {
+      this.props.updateSteps({step: step + 1})
+      this.props.history.push(`${this.props.match.url}/${step + 1}`)
+    }
+
+  }
+  onPrevious = () => {
+    // console.log(this.props.estimator)
+    const { step } = this.props.estimator
+    if (step <= 0) {
+      this.props.updateSteps({step: 1})
+      this.props.history.push(`${this.props.match.url}/${1}`)
+    } else if (step === 1) {
+      this.props.history.push(`/Dose`)
+    } else {
+      this.props.updateSteps({step: step - 1})
+      this.props.history.push(`${this.props.match.url}/${step - 1}`)
+    }
+  }
   render() {
     return (
       <div>
