dc6be6a73629e0683e677ee4bd9b94e1c2ba3b72
Darth Varg <cbvargas123@gmail.com>
2020-09-02 22:28:30 -0700

    added severity rendering section to component

--- client/src/components/pages/Dose/Survey/TabWeightSeverity.jsx
+++ client/src/components/pages/Dose/Survey/TabWeightSeverity.jsx
@@ -1,11 +1,34 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default class TabWeightSeverity extends Component {
+import { getSeveritiesForSurvey, inputWeight } from '../../../../actions/survey'
+
+class TabWeightSeverity extends Component {
+  componentDidMount() {
+    this.props.getSeveritiesForSurvey()
+  }
+  handleSelection = (e) => {
+    console.log(e.target.name)
+    // this.props.selectSeverity({severityId: e.target.name})
+  }
   render() {
     return (
-      <div>
-        
-      </div>
+      <section className="tab severity-tab survey-severity">
+        <div>
+          <p className="instructions">Select the severity of your condition:*</p>
+          <ul>
+            {this.props.severities.map(({ severity, id }) => (
+              <li key={id}>
+                <button className="severity-btn" onClick={this.handleSelection} name={severity}><img name={severity} src={`..\\..\\images\\Buttons_icons\\Severity options\\${severity}.png`} alt={`${severity} severity`}/>{severity}</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
     )
   }
 }
+
+const mapStateToProps = (state) => ({...state.survey})
+
+export default connect(mapStateToProps, { getSeveritiesForSurvey, inputWeight })(TabWeightSeverity)
