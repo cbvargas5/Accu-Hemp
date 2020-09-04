31d8d061046ed3b2441faec8c74b048b0f72dca2
Darth Varg <cbvargas123@gmail.com>
2020-09-04 16:21:14 -0700

    created files for advertising products

---
+++ client/src/components/pages/AdvertiseProducts/AdvertiseProducts.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AdvertiseProducts extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
---
+++ client/src/components/pages/AdvertiseProducts/TabUserInfo.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TabUserInfo extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
