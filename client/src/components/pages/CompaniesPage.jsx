a871f57a801f0dee5fbf133a9d5c831a664fe582
Darth Varg <cbvargas123@gmail.com>
2020-09-04 15:56:55 -0700

    added jsx to git st

--- client/src/components/pages/CompaniesPage.jsx
+++ client/src/components/pages/CompaniesPage.jsx
@@ -7,14 +7,28 @@ export default function CompaniesPage() {
       <section className="mini-header">
         <h1>Company Partnerships</h1>
       </section>
-      <section className="option-1">
+      <section className="option-1  underline-sec">
+        <h3>Basic Requirements:</h3>
+        <ul>
+          <li>
+            <i className="fas fa-check-circle"></i>
+            <span>Be a legitimate company</span>
+          </li>
+          <li>
+            <i className="fas fa-check-circle"></i>
+            <span>Have products containing CBD from legal hemp</span>
+          </li>
+          <li>
+            <i className="fas fa-check-circle"></i>
+            <span>Have products in dropper bottles or syringes</span>
+          </li>
+        </ul>
+      </section>
+      <section className="option-2 underline-sec">
 
       </section>
-      <section className="option-2">
+      <section className="right-to-deny underline-sec">
 
-      </section>
-      <section className="right-to-deny">
-        
       </section>
     </section>
   )
