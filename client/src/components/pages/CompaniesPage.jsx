fdfc9916beb550b8e12552df5796d6312f24415b
Darth Varg <cbvargas123@gmail.com>
2020-09-04 15:48:18 -0700

    added sections and buttoncard to component

--- client/src/components/pages/CompaniesPage.jsx
+++ client/src/components/pages/CompaniesPage.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import ButtonCard from '../cards/ButtonCard.jsx'
 
 export default function CompaniesPage() {
   return (
-    <section>
-      <h2>Companies</h2>
+    <section className="companies-page">
+      <section className="mini-header">
+        <h1>Company Partnerships</h1>
+      </section>
+      <section className="option-1">
+
+      </section>
+      <section className="option-2">
+
+      </section>
+      <section className="right-to-deny">
+        
+      </section>
     </section>
   )
 }
