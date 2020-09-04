f46634f647ca5063f83580e82852072556be389e
Darth Varg <cbvargas123@gmail.com>
2020-09-03 22:45:23 -0700

    added jsx to component

--- client/src/components/pages/Dose/Survey/TabSubmit.jsx
+++ client/src/components/pages/Dose/Survey/TabSubmit.jsx
@@ -13,7 +13,16 @@ class TabSubmit extends Component {
   render() {
     return (
       <section className="tab survey-submit-tab">
-        
+        <p className="instructions">Please verify that you understand this survey is anonymous and that you give us permission to collect and use the information you have filled out in this survey.*</p>
+        <ul>
+          <li>
+            <input onClick={this.handleSelection} name="Yes" type="button" value={'Yes, I understand and agree to these statements.'}/>
+          </li>
+          <li>
+            <input onClick={this.handleSelection} name="No" type="button" value={'No'}/>
+          </li>
+        </ul>
+        {/* add a navigate to dose page  */}
       </section>
     )
   }
