c6a29190fef8eb133604be4cbb77986f3c45502e
Darth Varg <cbvargas123@gmail.com>
2020-09-04 16:09:23 -0700

    completed base of jsx before buttoncards

--- client/src/components/pages/CompaniesPage.jsx
+++ client/src/components/pages/CompaniesPage.jsx
@@ -26,11 +26,35 @@ export default function CompaniesPage() {
       </section>
       <section className="option-1 underline-sec">
         <h2><strong>Option 1:</strong> Add Products to our Measurement Quiz</h2>
-
+        <p>Currently it is free to add your products to our Measurement quiz! Just fill out this form and attach a document or excel sheet with the product information.</p>
+        <ButtonCard link="/dose/survey" icon="fas fa-clipboard-list">CBD Dose Survery</ButtonCard>
       </section>
       <section className="option-2 underline-sec">
         <h2><strong>Option 2:</strong> We Recommend Your Products</h2>
-
+        <p>For Option 2 we would recommend your products through our Product Finder quiz and/or our Blog. For us to recommend your products on our site, we ask for a few additional requirements:</p>
+        <ul>
+          <li>
+            <i className="fas fa-laptop"></i>
+            <span>An online store we can refer customers to.</span>
+          </li>
+          <li>
+            <i className="fas fa-flask"></i>
+            <span>Quality testing with results easily accessible on your website.</span>
+          </li>
+          <li>
+            <i className="fas fa-dollar-sign"></i>
+            <span>Affiliate Program we can join.</span>
+          </li>
+          <li>
+            <i className="fas fa-handshake"></i>
+            <span>No Affiliate Program? We can work out an advertising partnership!</span>
+          </li>
+          <li>
+            <i className="fas fa-clipboard-list"></i>
+            <span>Fill out a quick application describing your brand and products.</span>
+          </li>
+        </ul>
+        <ButtonCard link="/dose/survey" icon="fas fa-clipboard-list">CBD Dose Survery</ButtonCard>
       </section>
       <section className="right-to-deny underline-sec">
         <h3>We have the right to deny or remove a brand from our website if:</h3>
