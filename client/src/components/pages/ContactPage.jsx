d1599d8c2ae1038675670ef1c86126e09e8860d8
Darth Varg <cbvargas123@gmail.com>
2020-09-04 17:24:42 -0700

    fixed logo padding

--- client/src/components/pages/ContactPage.jsx
+++ client/src/components/pages/ContactPage.jsx
@@ -15,15 +15,15 @@ export default function ContactPage() {
         <section className="form-wrapper">
           <form className="contact-form">
             <div className="name-field-wrapper">
-              <label for="form-name">Name</label>
+              <label htmlFor="form-name">Name</label>
               <input type="text" name="form-name" id="form-name"/>
             </div>
             <div className="email-field-wrapper">
-              <label for="form-email">Email</label>
+              <label htmlFor="form-email">Email</label>
               <input type="email" required="required" name="form-email" id="form-email"/>
             </div>
             <div className="message-field-wrapper">
-              <label for="form-message">Message</label>
+              <label htmlFor="form-message">Message</label>
               <textarea name="form-message" id="form-message" rows="4"></textarea>
             </div>
             <Button type="submit">Send</Button>
