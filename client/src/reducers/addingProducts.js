149453a0d3c722152c2146252ad820f351e25d58
Darth Varg <cbvargas123@gmail.com>
2020-09-05 21:06:11 -0700

    applied constants to reducer

--- client/src/reducers/addingProducts.js
+++ client/src/reducers/addingProducts.js
@@ -1,4 +1,4 @@
-// import { AGREE } from '../constants/action-types'
+import { UPDATE_ADDING_PRODUCTS_STEPS, UPDATE_INPUT_COMPANY_NAME, SELECT_INVOLVEMENT, UPDATE_INPUT_EMAIL, UPDATE_INPUT_WEBSITE } from '../constants/action-types'
 
 
 const initialState = {
@@ -12,7 +12,19 @@ const initialState = {
 export default (state = initialState, { type, payload }) => {
     switch (type) {
 
-        case 'AGREE':
+        case UPDATE_ADDING_PRODUCTS_STEPS:
+            return {...state, ...payload }
+
+        case UPDATE_INPUT_COMPANY_NAME:
+            return {...state, ...payload }
+
+        case SELECT_INVOLVEMENT:
+            return {...state, ...payload }
+
+        case UPDATE_INPUT_EMAIL:
+            return {...state, ...payload }
+
+        case UPDATE_INPUT_WEBSITE:
             return {...state, ...payload }
 
         default:
