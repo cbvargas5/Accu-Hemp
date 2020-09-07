7984057b4859a4efc7392056899e822403fd65d1
Darth Varg <cbvargas123@gmail.com>
2020-09-06 20:50:44 -0700

    created constants and added them to reducer

--- client/src/reducers/advertising.js
+++ client/src/reducers/advertising.js
@@ -1,4 +1,4 @@
-// import { UPDATE_ADDING_PRODUCTS_STEPS, UPDATE_INPUT_COMPANY_NAME, SELECT_INVOLVEMENT, UPDATE_INPUT_EMAIL, UPDATE_INPUT_WEBSITE } from '../constants/action-types'
+import { UPDATE_ADVERTISING_STEPS, UPDATE_INPUT_COMPANY_NAME_ADVERTISING, SELECT_INVOLVEMENT_ADVERTISING, UPDATE_INPUT_FIRST_NAME, UPDATE_INPUT_LAST_NAME, UPDATE_INPUT_EMAIL_ADVERTISING, UPDATE_INPUT_PHONE, UPDATE_INPUT_SHOP_WEBSITE, UPDATE_INPUT_QUALITY_LINK, SELECT_AFFILIATE_STATUS, UPDATE_AFFILIATE_APP_LINK, UPDATE_INPUT_BRAND_DESC } from '../constants/action-types'
 
 
 const initialState = {
@@ -8,21 +8,43 @@ const initialState = {
 export default (state = initialState, { type, payload }) => {
     switch (type) {
 
-        // case UPDATE_ADDING_PRODUCTS_STEPS:
-        //     return {...state, ...payload }
+        case UPDATE_ADVERTISING_STEPS:
+            return {...state, ...payload }
 
-        // case UPDATE_INPUT_COMPANY_NAME:
-        //     return {...state, ...payload }
+        case UPDATE_INPUT_COMPANY_NAME_ADVERTISING:
+            return {...state, ...payload }
 
-        // case SELECT_INVOLVEMENT:
-        //     return {...state, ...payload }
+        case SELECT_INVOLVEMENT_ADVERTISING:
+            return {...state, ...payload }
 
-        // case UPDATE_INPUT_EMAIL:
-        //     return {...state, ...payload }
+        case UPDATE_INPUT_FIRST_NAME:
+            return {...state, ...payload }
 
-        // case UPDATE_INPUT_WEBSITE:
-        //     return {...state, ...payload }
+        case UPDATE_INPUT_LAST_NAME:
+            return {...state, ...payload }
 
-        default: return state
+        case UPDATE_INPUT_EMAIL_ADVERTISING:
+            return {...state, ...payload }
+
+        case UPDATE_INPUT_PHONE:
+            return {...state, ...payload }
+
+        case UPDATE_INPUT_SHOP_WEBSITE:
+            return {...state, ...payload }
+
+        case UPDATE_INPUT_QUALITY_LINK:
+            return {...state, ...payload }
+
+        case SELECT_AFFILIATE_STATUS:
+            return {...state, ...payload }
+
+        case UPDATE_AFFILIATE_APP_LINK:
+            return {...state, ...payload }
+
+        case UPDATE_INPUT_BRAND_DESC:
+            return {...state, ...payload }
+
+        default:
+            return state
     }
 }
