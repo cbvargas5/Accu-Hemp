f9e83a0baee67a1cb0fc39a569c16acc2325dd8f
Darth Varg <cbvargas123@gmail.com>
2020-09-10 23:50:31 -0700

    installed helmet and added to server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const helmet = require("helmet");
 const port = process.env.PORT || 3131;
 const path = require('path');
 require('dotenv').config();
@@ -9,7 +10,7 @@ const PRODUCTS = require('./routers/products')
 
 // main().catch(console.error);
 
-
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../client/dist')));
