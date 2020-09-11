bf3475b3dd11d3edbd10e4c545aaa6bfa4f80712
Darth Varg <cbvargas123@gmail.com>
2020-09-11 14:35:32 -0700

    completed component jsx

--- client/src/components/pages/AdvertiseProducts/TabSubmitAdvertise.jsx
+++ client/src/components/pages/AdvertiseProducts/TabSubmitAdvertise.jsx
@@ -5,8 +5,15 @@ import { updateAffiliateLink, updateInputBrandDesc } from '../../../actions/adve
 class TabSubmitAdvertise extends Component {
   render() {
     return (
-      <section>
-        Submit!
+      <section className="tab link-questions-tab">
+        <div>
+          <p className="instructions">Website for your Affiliate Program application:</p>
+          <input onChange={this.handleChange} type="url" name="affiliate-program-app" id="affiliate-program-app" placeholder="http://"/>
+        </div>
+        <div>
+          <p className="instructions">Pleae tell us a little about your brand and your products:*</p>
+          <textarea onChange={this.handleChange} type="text" name="brand-description" id="brand-description" placeholder="For example: How long have you been in business? Where do you source your products? Any certifications? Special features? etc."/>
+        </div>
       </section>
     )
   }
