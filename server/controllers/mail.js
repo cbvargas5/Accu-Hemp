f24f4473abe71929eecde1c4668b1914d06ab8c9
Darth Varg <cbvargas123@gmail.com>
2020-09-12 23:52:34 -0700

    comments for mail file

--- server/controllers/mail.js
+++ server/controllers/mail.js
@@ -12,9 +12,21 @@ module.exports = {
             .then(() => res.status(200).end())
             .catch(err => res.status(400).send(err))
     },
+
+    // sendDropperResults: (req, res) => {
+    //     sendEmailToClient(req.body, 'estimator')
+    //         .then(() => res.status(200).end())
+    //         .catch(err => res.status(400).send(err))
+    // },
+
+    // sendSyringeResults: (req, res) => {
+    //     sendEmailToClient(req.body, 'estimator')
+    //         .then(() => res.status(200).end())
+    //         .catch(err => res.status(400).send(err))
+    // },
 }
 
 /**
- * Fields for [Estimator]:
+ * Fields for [Dropper/Syringe]:
  * lowerdose, upperdose, email
  */
