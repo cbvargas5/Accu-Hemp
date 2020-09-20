a8b523499d786e8c6ffcdd03f23cfdb0690d6c57
Darth Varg <cbvargas123@gmail.com>
2020-09-19 18:54:02 -0700

    created test jsx for file upload

--- client/src/components/pages/AddProducts/TabDragAndDrop.jsx
+++ client/src/components/pages/AddProducts/TabDragAndDrop.jsx
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
+import Axios from 'axios';
 
 export default class TabDragAndDrop extends Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      file:null
+    }
+  }
+  handleUpload = (e) => {
+    console.log(e.target.files[0])
+  }
   render() {
     return (
       <div>
         Drag and Drop
+        <form onSubmit={this.onFormSubmit}>
+          <h1>File Upload(TEST)</h1>
+          <input type="file" onChange={this.handleUpload} />
+          <button type="submit">Upload</button>
+      </form>
       </div>
     )
   }
