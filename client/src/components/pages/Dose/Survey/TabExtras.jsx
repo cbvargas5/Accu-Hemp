ef667dabb294de183af0e71c991378c7b7e01485
Darth Varg <cbvargas123@gmail.com>
2020-09-23 14:50:25 -0700

    applied required fields where applicable

--- client/src/components/pages/Dose/Survey/TabExtras.jsx
+++ client/src/components/pages/Dose/Survey/TabExtras.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { selectSurveyDose, selectProfessionalHelp, selectOtherMedication, updateOtherMedicationInput } from '../../../../actions/survey'
+import { selectSurveyDose, selectProfessionalHelp, selectOtherMedication, updateOtherMedicationInput, updateValidationError } from '../../../../actions/survey'
 
 
 class TabExtras extends Component {
@@ -9,40 +9,46 @@ class TabExtras extends Component {
   }
   handleChange = (e) => {
     const { name,value } = e.target
+    if (this.props.validationError) {
+      this.props.updateValidationError({validationError: false})
+    }
     switch(name) {
       case 'dose':
         this.props.selectSurveyDose({selectedDose: value})
-        return
+        break;
       case 'other medications explanation':
         this.props.updateOtherMedicationInput({otherMedicationInput: value})
-        return
+        break;
       default:
-        return
+        break;
     }
   }
   handleSelection = (e) => {
     const { name,value } = e.target
+    if (this.props.validationError) {
+      this.props.updateValidationError({validationError: false})
+    }
     switch(name) {
       case 'professional':
         this.props.selectProfessionalHelp({selectedProfessionalHelp: value})
-        return
+        break;
       case 'other medications':
         this.props.selectOtherMedication({selectedOtherMedication: value})
-        return
+        break;
       default:
-        return
+        break;
     }
 
   }
   render() {
     return (
       <section className="tab extras-tab">
-        <div>
-          <p className="instructions">How many milligrams of CBD do you take for your condition?*</p>
+        <div className={this.props.validationError ? "highlight-error" : ""}>
+          <p className="instructions required-field">How many milligrams of CBD do you take for your condition?</p>
           <input onChange={this.handleChange} type="number" name="dose" id="dose"/>
         </div>
-        <div>
-          <p className="instructions">Did a licensed medical professional help you figure out your dose of CBD?*</p>
+        <div className={this.props.validationError ? "highlight-error" : ""}>
+          <p className="instructions required-field">Did a licensed medical professional help you figure out your dose of CBD?</p>
           <ul>
             <li>
               <input onClick={this.handleSelection} name="professional" type="button" value={'Yes'}/>
@@ -55,8 +61,8 @@ class TabExtras extends Component {
             </li>
           </ul>
         </div>
-        <div>
-          <p className="instructions">Is there anything else you are taking for your condition?*</p>
+        <div className={this.props.validationError ? "highlight-error" : ""}>
+          <p className="instructions required-field">Is there anything else you are taking for your condition?</p>
           <span>i.e. pharmaceuticals, over the counter medications</span>
           <ul>
             <li>
@@ -70,8 +76,7 @@ class TabExtras extends Component {
             </li>
           </ul>
         </div>
-        {/* can change this when I create a class for this div */}
-        <div style={this.props.selectedOtherMedication === 'Yes' ? {display:"block"} : {display:"none"}}>
+        <div className={this.props.selectedOtherMedication == 'Yes' ? "" : "hide"}>
           <p className="instructions">Other than CBD hemp oil, what else are you taking for your condition?</p>
           <textarea onChange={this.handleChange} name="other medications explanation" id="other-medications" cols="50" rows="5"></textarea>
         </div>
@@ -83,4 +88,4 @@ class TabExtras extends Component {
 
 const mapStateToProps = (state) => ({...state.survey})
 
-export default connect(mapStateToProps, { selectSurveyDose, selectProfessionalHelp, selectOtherMedication, updateOtherMedicationInput })(TabExtras)
+export default connect(mapStateToProps, { selectSurveyDose, selectProfessionalHelp, selectOtherMedication, updateOtherMedicationInput, updateValidationError })(TabExtras)
