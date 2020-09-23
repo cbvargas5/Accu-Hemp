922c52a04e1627dab944243f7eb82cb504566cc5
Darth Varg <cbvargas123@gmail.com>
2020-09-22 17:55:30 -0700

    created component for error message

---
+++ client/src/components/SubmissionError.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function SubmissionError() {
+  return (
+    <div>
+      <div className="error-msg">
+        There was a problem with your submission. Errors have been highlighted below.
+      </div>
+    </div>
+  )
+}
