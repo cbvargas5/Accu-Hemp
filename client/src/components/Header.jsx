38c1897055b31cb6b439dfd4eca75a37ef89082f
Darth Varg <cbvargas123@gmail.com>
2020-10-01 17:03:56 -0700

    created simple functioning hamburger menu

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './Navbar.jsx'
 import { Link } from 'react-router-dom';
 
-const Header = (props) => (
-  <header>
-    <div className="header-logo-wrapper">
-      <div className="image-wrapper">
-        <Link to="/"><img src="./images/main-logo.png" alt="Accu-Hemp Main Logo"/></Link>
+const Header = (props) => {
+  const [click, setClick] = useState('')
+  const handleClick = (e) => {
+    click ? setClick('') : setClick('change')
+  }
+  return (
+    <header>
+      <div className="header-logo-wrapper">
+        <div className="image-wrapper">
+          <Link to="/"><img src="./images/main-logo.png" alt="Accu-Hemp Main Logo"/></Link>
+        </div>
       </div>
-    </div>
-    <Navbar />
-  </header>
-);
+      <Navbar />
+      <div className={`hamburger-container ${click}`} onClick={handleClick}>
+        <div className="bar1"></div>
+        <div className="bar2"></div>
+        <div className="bar3"></div>
+      </div>
+    </header>
+)};
 
 export default Header
