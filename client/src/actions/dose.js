075cec4e7b86c728ea4ee00965e6a5df2eff65eb
Darth Varg <cbvargas123@gmail.com>
2020-10-11 23:21:40 -0700

    updated endpoints for name change

--- client/src/actions/dose.js
+++ client/src/actions/dose.js
@@ -2,21 +2,21 @@ import { GET_CONDITIONS, UPDATE_STEPS, SELECT_CONDITION, GET_SEVERITIES, UPDATE_
 import axios from 'axios'
 
 export const getConditions = () => (dispatch, getState) => {
-    return axios.get('/dosages/conditions')
+    return axios.get('/api/dosages/conditions')
         .then(({ data }) => dispatch({ type: GET_CONDITIONS, payload: data }))
         .catch(err => console.log('failed to get conditions. ERROR: ', err))
 }
 
 export const getSeverities = () => (dispatch, getState) => {
     const { estimator } = getState()
-    return axios.get(`/dosages/${estimator.selectedCondition}`)
+    return axios.get(`/api/dosages/${estimator.selectedCondition}`)
         .then(({ data }) => dispatch({ type: GET_SEVERITIES, payload: data }))
         .catch(err => console.log('failed to get severities. ERROR: ', err))
 }
 
 export const getDose = () => (dispatch, getState) => {
     const { estimator } = getState()
-    return axios.get(`/dosages/dose/${estimator.severityId}`)
+    return axios.get(`/api/dosages/dose/${estimator.severityId}`)
         .then(({ data }) => dispatch({ type: GET_DOSE, payload: data }))
         .catch(err => console.log('failed to get doses. ERROR: ', err))
 }
